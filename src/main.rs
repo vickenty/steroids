52b2dfd7188e8bc681e5c4a066c3272fd18dc45a
Pavel Gurkov <pavel.gurkov@booking.com>
2017-08-04 17:36:21 +0200

    WIP: Entities

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate nphysics3d;
 extern crate ncollide;
 
 use std::collections::HashMap;
+//use std::borrow::BorrowMut;
+//use std::borrow::Borrow;
 
 struct Controller {
     pu: three::Button,
@@ -61,10 +63,20 @@ struct Entity {
     mesh: three::Mesh,
 }
 
-impl Entity {
-    fn new(window: &mut three::Window, world: &mut nphysics3d::world::World<f32>, x: f32, y: f32, z: f32) -> Self {
+struct Cube {
+    //    body: nphysics3d::object::RigidBodyHandle<f32>,
+    //    mesh: three::Mesh,
+    entity: Entity,
+}
+
+impl Ent for Cube {
+    fn new(window: &mut three::Window, world: &mut nphysics3d::world::World<f32>, properties: HashMap<&'static str, f32>) -> Box<Ent> {
         let shape = ncollide::shape::Cuboid::new(nphysics3d::math::Vector::new(0.5, 0.5, 0.5));
         let mut body = nphysics3d::object::RigidBody::new_dynamic(shape, 1.0, 1.0, 1.0);
+        let x = properties.get("x").cloned().unwrap();
+        let y = properties.get("y").cloned().unwrap();
+        let z = properties.get("z").cloned().unwrap();
+
         body.set_translation(nphysics3d::math::Translation::new(x, y, z));
         let hndl = world.add_rigid_body(body);
 
@@ -72,39 +84,99 @@ impl Entity {
         let mesh = window.factory.mesh(geom, three::Material::MeshLambert { color: 0xabcdef, flat: true });
         window.scene.add(&mesh);
 
-        Entity {
-            body: hndl,
-            mesh: mesh,
-        }
+        Box::new(
+            Cube {
+                entity:
+                Entity {
+                    body: hndl,
+                    mesh: mesh,
+                }
+            })
+    }
+    fn get_body(&self) -> nphysics3d::object::RigidBodyHandle<f32> {
+        self.entity.body.clone()
     }
 
+    fn get_mesh(&mut self) -> &mut three::Mesh {
+        &mut self.entity.mesh
+    }
+}
+
+trait Ent {
+    fn new(window: &mut three::Window, world: &mut nphysics3d::world::World<f32>, properties: HashMap<&'static str, f32>) -> Box<Ent>
+        where Self: Sized;
+
+    fn get_body(&self) -> nphysics3d::object::RigidBodyHandle<f32>;
+
+    fn get_mesh(&mut self) -> &mut three::Mesh;
+
     fn update_body(&self) {
-        self.body.borrow_mut().clear_forces();
+        let body = self.get_body();
+        body.borrow_mut().clear_forces();
     }
 
     fn update_mesh(&mut self) {
-        let body = self.body.borrow();
+        let body = self.get_body();
+        let body = body.borrow();
         let pos = body.position();
 
         let pf: [f32; 3] = pos.translation.vector.into();
         let rf: [f32; 4] = pos.rotation.as_ref().coords.into();
 
-        self.mesh.set_transform(pf, rf, 1.0);
+        self.get_mesh().set_transform(pf, rf, 1.0);
     }
 
-    fn look_at<P>(&self, camera: &mut three::Camera<P>) {
-        let pf: [f32; 3] = self.body.borrow().position().translation.vector.into();
+    fn look_at(&self, camera: &mut three::Camera<three::Perspective>) {
+        let body = self.get_body();
+        let pf: [f32; 3] = body.borrow().position().translation.vector.into();
         camera.look_at([5.0, 5.0, 5.0], pf, None);
     }
 }
 
+//impl Entity {
+//    fn new(window: &mut three::Window, world: &mut nphysics3d::world::World<f32>, x: f32, y: f32, z: f32) -> Self {
+//        let shape = ncollide::shape::Cuboid::new(nphysics3d::math::Vector::new(0.5, 0.5, 0.5));
+//        let mut body = nphysics3d::object::RigidBody::new_dynamic(shape, 1.0, 1.0, 1.0);
+//        body.set_translation(nphysics3d::math::Translation::new(x, y, z));
+//        let hndl = world.add_rigid_body(body);
+//
+//        let geom = three::Geometry::new_box(1.0, 1.0, 1.0);
+//        let mesh = window.factory.mesh(geom, three::Material::MeshLambert { color: 0xabcdef, flat: true });
+//        window.scene.add(&mesh);
+//
+//        Entity {
+//            body: hndl,
+//            mesh: mesh,
+//        }
+//    }
+//
+//    fn update_body(&self) {
+//        self.body.borrow_mut().clear_forces();
+//    }
+//
+//    fn update_mesh(&mut self) {
+//        let body = self.body.borrow();
+//        let pos = body.position();
+//
+//        let pf: [f32; 3] = pos.translation.vector.into();
+//        let rf: [f32; 4] = pos.rotation.as_ref().coords.into();
+//
+//        self.mesh.set_transform(pf, rf, 1.0);
+//    }
+//
+//    fn look_at<P>(&self, camera: &mut three::Camera<P>) {
+//        let pf: [f32; 3] = self.body.borrow().position().translation.vector.into();
+//        camera.look_at([5.0, 5.0, 5.0], pf, None);
+//    }
+//}
+
 struct Registry {
     counter: u64,
-    entities: HashMap<u64, Entity>,
+    entities: HashMap<u64, Box<Ent>>,
 }
 
 impl Registry {
-    fn new() -> Self {
+    fn new() -> Registry {
         Registry {
             counter: 0,
             entities: HashMap::new(),
@@ -115,19 +187,24 @@ impl Registry {
         let id = self.counter;
         self.counter += 1;
 
-        let ent = Entity::new(window, world, x, y, z);
+        let mut properties: HashMap<&'static str, f32> = HashMap::new();
+        properties.insert("x", x);
+        properties.insert("y", y);
+        properties.insert("z", z);
+
+        let ent = Cube::new(window, world, properties);
         self.entities.insert(id, ent);
 
         id
     }
 
-    fn apply_one<F: FnMut(&mut Entity)>(&mut self, id: u64, mut f: F) {
+    fn apply_one<F: FnMut(&mut Box<Ent>)>(&mut self, id: u64, mut f: F) {
         if let Some(ent_ref) = self.entities.get_mut(&id) {
             f(ent_ref)
         }
     }
 
-    fn apply_all<F: FnMut(&mut Entity)>(&mut self, mut f: F) {
+    fn apply_all<F: FnMut(&mut Box<Ent>)>(&mut self, mut f: F) {
         for e in self.entities.values_mut() {
             f(e)
         }
@@ -172,7 +249,7 @@ fn main() {
 
     while window.update() {
         entities.apply_all(|e| e.update_body());
-        entities.apply_one(player_id, |e| control.update(&e.body, &window.input));
+        entities.apply_one(player_id, |e| control.update(&e.get_body(), &window.input));
 
         world.step(0.017);
 
